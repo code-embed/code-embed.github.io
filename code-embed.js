631428cf8860f9351c79fd72eb98f50978684f97
Danny Engelman <danny@engelman.nl>
2025-06-11 20:50:59 +0200

    v3

--- code-embed.js
+++ code-embed.js
@@ -1,6 +1,6 @@
 // Generic Web Component loader for NA-ME.github.io hosting
 // <script src="https://NA-ME.github.io/name.js">
-// Load multiple files ?file=pro,foo,bar
+// Load multiple files ?files=pro,foo,bar&type=module&async=false
 ((
     scriptSrc = document.currentScript.src,
     params = new URL(scriptSrc).searchParams,
@@ -10,14 +10,15 @@
     minified = filename[1] == "min" ? filename[1] : "", // if src is minified call all minified files
     prefix = location.hostname.includes("github.io") ? `https://${params.get("host") || name}.github.io/` : "",
     scripts = [`${name}-baseclass${minified}.js`],
+    async = params.get("async") || true,
 ) => {
     if (files) scripts.push(...files.split(",").map(module => `${name}-${module}${minified}.js`))
     document.head.append(...scripts.map((src, idx) => Object.assign(document.createElement("script"), {
         title: "injected",
         src: prefix + src,
-        async: params.get("async") || true,
+        async, // default async:true
         [params.get("type") ? "type" : "notype"]: params.get("type") ? params.get("type") : "module",
-        onload: () => console.log(`%c Loaded (${async ? "A" : ""}${idx}) %c %s `, "background:gold;color:black", "background:green;color:white", src),
+        onload: () => console.log(`%c Loaded (${async ? "async " : ""}${idx}) %c %s `, "background:gold;color:black", "background:green;color:white", src),
         onerror: () => console.log(`%c Failed (${idx}) %c %s `, "background:red;color:black", "background:green;color:white", src)
     })));
     customElements.whenDefined(name + "-baseclass").then(B => !files && customElements.define(name, class extends B { }));
