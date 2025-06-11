e9d9a360fb7cb2da8f3d30a5e831808bda0e5c21
Danny Engelman <danny@engelman.nl>
2025-06-11 17:56:02 +0200

    v1

--- code-embed.js
+++ code-embed.js
@@ -1,14 +1,21 @@
+// Generic Web Component loader for NA-ME.github.io hosting
+// <script src="https://NA-ME.github.io/name.js">
+// Loading:
+//  - NA-ME-baseclass.js
+//  - NA-ME-pro.js (optional)
 ((
-    s = document.currentScript.src,
-    v = new URL(s).searchParams.get("v"),
-    f = s.split("/").pop().split("."),
-    n = f[0],
-    m = f[1] == "min" ? f[1] : ""
+    scriptSrc = document.currentScript.src,
+    version = new URL(scriptSrc).searchParams.get("v"), // or undefined
+    filename = scriptSrc.split("/").pop().split("."),
+    name = filename[0],
+    minified = filename[1] == "min" ? filename[1] : "",
+    prefix = location.hostname.includes("github.io") ? `https://${name}.github.io/` : ""
 ) => {
-    document.head.append(...[n + `-baseclass${m}.js`, ...(v ? [n + `-${v}${m}.js`] : [])]
+    console.log(location.hostname)
+    document.head.append(...[name + `-baseclass${minified}.js`, ...(version ? [name + `-${version}${minified}.js`] : [])]
         .map(src => Object.assign(document.createElement("script"), {
-            title: "injected", src, async: true, type: "module", onload: () => v && console.log("Loaded:", src)
+            title: "injected", src: prefix + src, async: true, type: "module", onload: () => version && console.log("Loaded:", src)
         })));
-    customElements.whenDefined(n + "-baseclass").then(B => !v && customElements.define(n, class extends B { })
+    customElements.whenDefined(name + "-baseclass").then(B => !version && customElements.define(name, class extends B { })
     );
 })();
